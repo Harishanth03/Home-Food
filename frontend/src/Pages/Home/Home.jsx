82fcb20c3b80e7e0489dfb9afbad37841c665209
Harishanth03 <kandeepanharishanth@gmail.com>
2024-11-07 23:35:48 +0530

    Insert Images and Explore Menu design

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 
 import './Home.css'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
    <div>
 
    <Header/>
+
+   <ExploreMenu/>
 
    </div>
   )
