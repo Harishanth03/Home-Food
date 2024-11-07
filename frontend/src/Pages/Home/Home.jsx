4ff6171ed47fbe021ae4d0e08896102873fe58ec
Harishanth03 <kandeepanharishanth@gmail.com>
2024-11-07 08:28:01 +0530

    Header Commponent

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import Header from '../../components/Header/Header'
+
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <div>
+
+   <Header/>
+
+   </div>
   )
 }
 
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+
+  return (
+
+    <section className='section-header'>
+
+        <div className='header'>
+
+            <div className="header-content">
+
+                <h2>Order your favourite home food here!</h2>
+
+                <p>Craving a taste of home? Discover delicious, freshly-made dishes crafted with love and delivered right to your door. Dive into the comfort of homemade flavors today!</p>
+
+                <button>View Menu</button>
+
+            </div>
+
+        </div>
+
+    </section>
+
+  )
+
+}
+
+export default Header
