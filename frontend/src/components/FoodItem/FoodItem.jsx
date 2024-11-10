9bfbfee1026a54b4e88d0f12b51449f500447d06
Harishanth03 <kandeepanharishanth@gmail.com>
2024-11-10 11:47:16 +0530

    created Counter

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './FoodItems.css'
 
 import {assets} from '../../assets/assets'
 
 const FoodItem = ({id , name , price , description , image}) => {
+
+  const [itemCount , setItemCount] = useState(0);
 
   return (
 
@@ -13,6 +15,22 @@ const FoodItem = ({id , name , price , description , image}) => {
       <div className="food-item-img-container">
 
         <img className='food-item-image' src={image}alt="" srcset="" />
+
+        {!itemCount 
+
+          ?<img className='add' onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white}/>
+
+          :<div className='food-item-counter'>  
+
+            <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red}/>
+
+            <p>{itemCount}</p>
+
+            <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green}alt="" />
+
+          </div>
+
+        }
         
       </div>
 
