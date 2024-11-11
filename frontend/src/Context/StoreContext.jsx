d01dc917d2bc9faf4c71bcc868a7a5f6a9750fca
Harishanth03 <kandeepanharishanth@gmail.com>
2024-11-11 21:59:37 +0530

    add to cart

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,10 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+    const [cartItem , storecartItem] = useState({});
+
+    const addToCart = itemId => 
+    {
+        if(!cartItem[itemId])
+        {
+            storecartItem((prev) => ({...prev, [itemId]:1})) //this function return an object
+        }
+        else
+        {
+            storecartItem((prev) => ({...prev,[itemId]:prev[itemId] + 1})) // get the all data using spread operator and select the prev quantity using Id and add + 1 to the cart
+        }
+    }
 
 
     const contextValue = {
