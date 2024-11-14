820af83153dfb39c215796e7da8235b20fc297cc
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-14 09:06:42 +0530

    Media query for header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,23 +6,19 @@ const Header = () => {
 
   return (
 
-    <section className='section-header'>
+    <div className='header'>
 
-        <div className='header'>
+      <div className="header-content">
 
-            <div className="header-content">
+        <h2>Order your favourite home food here!</h2>
 
-                <h2>Order your favourite home food here!</h2>
+        <p>Craving a taste of home? Discover delicious, freshly-made dishes crafted with love and delivered right to your door. Dive into the comfort of homemade flavors today!</p>
 
-                <p>Craving a taste of home? Discover delicious, freshly-made dishes crafted with love and delivered right to your door. Dive into the comfort of homemade flavors today!</p>
+        <button>View Menu</button>
 
-                <button>View Menu</button>
+      </div>
 
-            </div>
-
-        </div>
-
-    </section>
+    </div>
 
   )
 
