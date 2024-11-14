3cbf33befadf6c985baf258a71a2c308f256fc99
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-14 12:54:29 +0530

    start Cart

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import './Cart.css'
 
+import { StoreContext } from '../../Context/StoreContext'
+
 const Cart = () => {
+
+  const {food_list , cartItem ,removeFromCart} = useContext(StoreContext);
+
   return (
-    <div>Cart</div>
+
+    <div>
+
+      
+
+    </div>
+
   )
+
 }
 
 export default Cart
