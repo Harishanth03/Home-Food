b759d000075a319560621968e28d60a97db5d5f4
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-23 18:51:45 +0530

    Create Food COntroller File

---
+++ backend/controllers/foodControllers.js
@@ -0,0 +1,11 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+//====================================================== ADD FOOD ITEMS ===========================================================
+
+const addFood = async (request , response) => {
+
+
+};
+
+export {addFood};
