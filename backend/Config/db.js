70741cfefe19b4e86da3f7adec004fdcb86163d6
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-23 18:21:05 +0530

    Create Food Model and Set error handling to database

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -2,5 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
 
-    await mongoose.connect('mongodb+srv://harishanth03:H200018503118h@cluster0.plbv9.mongodb.net/home-food').then(() => console.log('DB connected'));
+    try
+    {
+        await mongoose.connect('mongodb+srv://harishanth03:H200018503118h@cluster0.plbv9.mongodb.net/home-food').then(() => console.log('DB connected'));
+    }
+    catch(error)
+    {
+        console.log('DB Connection Failed' , error.message);
+    }
 }
