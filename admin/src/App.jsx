648e7e9ef7770c1f3143cee40e1b2200143087f2
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-26 22:14:09 +0530

    Set BrowserRouter

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import NavBar from './components/Navbar/NavBar'
+import SideBar from './components/SideBar/SideBar'
 
 const App = () => {
   return (
     <div>
       
+      <NavBar/>
+
+      <hr />
+
+      <div className="app-content">
+
+        <SideBar/>
+        
+      </div>
     </div>
   )
 }
