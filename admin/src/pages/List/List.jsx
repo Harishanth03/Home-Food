6c0f2ac02df9d505468da52ba1ac569007e8ed99
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-28 10:19:47 +0530

    code for list

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 
 import { toast } from 'react-toastify';
 
+import { url } from '../../assets/assets';
+
 const List = () => {
 
   const GetURL = "http://localhost:4000"
@@ -26,18 +28,65 @@ const List = () => {
     {
       toast.error("Error");
     }
-  }
+  };
 
-  useEffect(() => {
+  useEffect( ()=>{
 
     fetchList();
 
-  }, [])
+  } , []);
+
+  const removeFood = async(food_List) => {
+
+    const response = await axios.post(`${url}/api/food/remove`, {id:food_List});
+
+    await fetchList();
+
+  };
+
+  
 
   return (
 
-    <div>
+    <div className='list add flex-col'>
 
+      <p>All food list</p>
+
+      <div className="list-table">
+
+        <div className="list-table-format title">
+
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+
+        </div>
+
+        {list.map((Item , index ) => {
+
+          return(
+
+            <div key={index} className='list-table-format remove'>
+
+              <img src={`${url}/images/` + Item.image} alt="" />
+
+              <p>{Item.name}</p>
+
+              <p>{Item.category}</p>
+
+              <p>LKR: {Item.price}</p>
+
+              <p onClick={() => removeFood(Item._id)} className='cursor'>X</p>
+
+            </div>
+
+          )
+
+        })}
+
+      </div>
 
     </div>
   )
