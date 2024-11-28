390c3bf60add4a5dbd5ef8ed22777dcd126f6b28
harishanth03 <kandeepanharishanth@gmail.com>
2024-11-28 23:17:31 +0530

    create controller function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,1 +1,23 @@
 import userModel from "../models/UserModel";
+
+import jsonWebToken from "jsonwebtoken"; //implement secure connection
+
+import bycrypt from "bcrypt"; // hash the password data
+
+import validator from "validator"; // validate the email
+
+//==================================================== LOGIN USER FUNCTION =============================================================
+
+const loginUser = async (request , response) => {
+
+
+}
+
+//=================================================== REGISTER USERBFUNCTION =========================================================
+
+const registerUser = async (request , response) => {
+
+
+}
+
+export {registerUser , loginUser}
