e46ef2e03b0d32386db028fdbfedab964bd105cd
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-11 22:10:50 +0530

    initionalize add , delete and get cart Function

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,24 @@
+import userModel from "../models/UserModel.js";
+
+//=========================================================== Add Items To User Carts =================================================
+
+const addTocart = async(request , response) => {
+
+
+
+}
+
+
+//========================================================= Remove Items From User carts ===========================================
+
+const removeCart = async(request , response) => {
+
+
+}
+
+//========================================================= Frtch User Cart Data ===========================================
+
+const getcart = async(request , response) => {
+
+    
+}
