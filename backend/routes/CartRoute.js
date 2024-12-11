ebac279e042d55fa834c407a0053d7b056053f24
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-11 22:23:19 +0530

    create the routes

---
+++ backend/routes/CartRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import { addTocart , removeCart , getcart } from "../controllers/cartController.js";
+
+const cartRouter = express.Router();
+
+//End points 
+ cartRouter.post("/add" , addTocart);
+
+ cartRouter.post('/remove' , removeCart);
+
+ cartRouter.post("/get" , getcart);
+
+
+ 
