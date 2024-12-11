eecc929bf31a1953953d522bb622d59e2934ae90
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-11 22:41:25 +0530

    set Authmiddleware function into the routes

--- backend/routes/CartRoute.js
+++ backend/routes/CartRoute.js
@@ -2,14 +2,16 @@ import express from "express";
 
 import { addTocart , removeCart , getcart } from "../controllers/cartController.js";
 
+import authMiddleWare from "../middleware/auth.js";
+
 const cartRouter = express.Router();
 
 //End points 
- cartRouter.post("/add" , addTocart);
+ cartRouter.post("/add" , authMiddleWare , addTocart);
 
- cartRouter.post('/remove' , removeCart);
+ cartRouter.post('/remove' , authMiddleWare , removeCart);
 
- cartRouter.post("/get" , getcart);
+ cartRouter.post("/get" , authMiddleWare , getcart);
 
 
 //===================================================== Export ====================================================================
