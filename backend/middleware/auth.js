db7b47838273c8ebfc0973f8eba53ccd9e6a9d55
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-11 22:51:56 +0530

    AUth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,31 @@ import jwt from "jsonwebtoken";
 
 const authMiddleWare = async(request , response , next) => {
 
+   const {token} = request.headers; 
 
+   if(!token)
+   {
+
+    response.json({success:false , message:"Not Authorixed Please Login Again!"} );
+
+   }
+
+   try 
+   {
+
+    const token_decode = jwt.verify(token , process.env.JWT_SECRET);
+
+    request.body.userId = token_decode.id;
+
+    next();
+
+   } catch (error) {
+
+    console.log(error);
+
+    response.json({success:false , message:"Error"});
+    
+   }
 }
 
 export default authMiddleWare;
