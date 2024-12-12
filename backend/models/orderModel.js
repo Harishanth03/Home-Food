e9d9068e6504c4cc33df86e9593491eb28102390
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-12 21:52:46 +0530

    create the Order Models

---
+++ backend/models/orderModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+    userId: {type:String , required:true},
+
+    items: {type:Array , required:true},
+
+    amount: {type:Number , required:true},
+
+    status: {type:String , required:true},
+
+    date: {type:Date , default:Date.now()},
+
+    payment: {type:Boolean , default:false}
+
+});
+
+const orderModels = mongoose.models.order || mongoose.model("order" , orderSchema);
+
+export default orderModels;
