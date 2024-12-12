6a78abbe6a5ca7296899ac7dc1ae04e65cdaab9a
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-12 18:30:07 +0530

    add to cart Function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -7,7 +7,7 @@ const addTocart = async(request , response) => {
     try 
     {
 
-        let userData = await userModel.findOne({_id:request.body.userId});
+        let userData = await userModel.findOne({_id:request.body.userId}); // findOne 
 
         let cartData = await userData.cartData;
 
@@ -20,11 +20,15 @@ const addTocart = async(request , response) => {
             cartData[request.body.itemId] += 1;
         }
 
+        await userModel.findByIdAndUpdate(request.body.userId , {cartData});
 
-        
+        response.json({success:true , message:"Added to cart"});
+   
     } catch (error) 
     {
-        
+        console.log(error);
+
+        response.json({success:false , message:"Error"})
     }
 
 }
@@ -46,3 +50,7 @@ const getcart = async(request , response) => {
 
 
 export {addTocart , removeCart , getcart}
+
+//findOne method use for find peticular doocument from the mongoDb , its method only available in Mongoose. its use find only one user
+
+// : this colon using because i matching the data with userId because mongoose query using : this rather than ====
