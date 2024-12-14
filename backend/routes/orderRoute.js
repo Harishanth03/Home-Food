32b3771bb7169fa9bed856c6383c4d2cb0dde569
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-14 19:00:03 +0530

    create endpoint route

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,3 +3,7 @@ import { placeOrder } from "../controllers/orderController.js";
 import express from "express"
 
 import authMiddleWare from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place" , authMiddleWare , placeOrder)
