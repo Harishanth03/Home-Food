7b2d249cdd015f72bf342d1bf909d96c0c5ec05c
harishanth03 <kandeepanharishanth@gmail.com>
2024-12-15 08:07:01 +0530

    Set ROute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import LoginPopUp from "./components/LoginPopUp/LoginPopUp";
 import Verify from "./Pages/Verify/Verify";
+import MyOrder from "./Pages/MyOrders/MyOrder";
 
 const App = () => {
 
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/Cart" element={<Cart />} />
           <Route path="/Order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify/>}/>
+          <Route path="/myorders" element={<MyOrder/>}/>
 
         </Routes>
 
--- frontend/src/Pages/MyOrders/MyOrder.jsx
+++ frontend/src/Pages/MyOrders/MyOrder.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import "./MyOrder";
+import { StoreContext } from '../../Context/StoreContext';
 
 const MyOrder = () => {
+
+    const {urlLocal , token} = useContext(StoreContext);
+
+    const [userData , setUserData] = useState([]);
+
   return (
-    <div>MyOrder</div>
+    <div>
+        
+
+    </div>
   )
 }
 
